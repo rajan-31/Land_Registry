b558838aa0b2ac1a8686bc32353d9471b03286f0
Rajan Khade <chiragkhade@gmail.com>
2021-01-19 23:15:25 +0530

    Add files via upload

---
+++ stringToByte32.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.7.0 < 0.9.0;
+contract converter {
+    function stringToBytes32(string memory source) public pure returns (bytes32 result) {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) {
+            return 0x0;
+        }
+        assembly {
+                result := mload(add(source, 32))
+        }
+    }
+}
